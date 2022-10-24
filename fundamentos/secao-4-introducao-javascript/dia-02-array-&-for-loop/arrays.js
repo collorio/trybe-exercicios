392bb7f3724422eca0c6f24bd64838986b4317af
Collorio <collorio@hotmail.com>
2022-10-24 18:42:03 -0300

    Exercícios que não foram nos ultimos commits

---
+++ fundamentos/secao-4-introducao-javascript/dia-02-array-&-for-loop/arrays.js
@@ -0,0 +1,9 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+console.log(menuServices);
+
+menu.push('Contato')
+console.log(menu)
---
+++ fundamentos/secao-4-introducao-javascript/dia-02-array-&-for-loop/for.js
@@ -0,0 +1,8 @@
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let i =0; i < groceryList.length;i+=1){
+//     console.log(groceryList[i])
+// }
+
+let soMany = 10;
+console.log('This is', soMany, 'times easier!');
